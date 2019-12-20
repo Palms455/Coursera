c2c4a6a4cc695456e26feab0fbec87829e1b73a1
Rustam <rustrnb@yandex.ru>
2019-12-20 16:44:14 +0500

    Week 2 request and Beautiful_soup

---
+++ Web_services/Week2/currency.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+from decimal import Decimal
+
+
+def convert(amount, cur_from, cur_to, date, requests):
+    response = requests.get(f'https://www.cbr.ru/scripts/XML_daily.asp?date_req={date}') 
+    soup = BeautifulSoup(response.content, 'xml')
+
+    second_val = soup.find('CharCode', text=cur_to).find_next_sibling('Value')
+    second_nominal = soup.find('CharCode', text=cur_to).find_next_sibling('Nominal')
+    if cur_from != 'RUR':
+        first_val = soup.find('CharCode', text=cur_from).find_next_sibling('Value')
+        first_nominal = soup.find('CharCode', text=cur_from).find_next_sibling('Nominal')
+        first_val_rub = (Decimal(first_val.text.replace(',','.'))/int(first_nominal.text)) * amount
+      ''
+    else:
+        first_val_rub = amount
+    
+    total_sum = first_val_rub/(Decimal(second_val.text.replace(',','.')) /int(second_nominal.text))  
+
+    return total_sum.quantize(Decimal("1.0000")) 
